179301df3428893e9705abc4f7218016536c1b38
Alexandre Teixeira <alexandre@loldesign.com.br>
2020-04-21 22:22:22 -0300

    Part2 exercise 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import Person from './Components/Person';
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', id: 0 }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+      id: persons.length + 1,
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log('event.target.value :>> ', event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input 
+          value={newName} 
+          onChange={handleNameChange}
+           />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => 
+          <Person key={person.id} person={person} />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ part2/phonebook/src/Components/Person.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+const Person = ({ person }) => {
+  return (
+    <p>{person.name}</p>
+  )
+}
+
+export default Person
