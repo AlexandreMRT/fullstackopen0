03e9af06324a4f0ede16434943f76bcbd96b2e8b
Alexandre Teixeira <alexandre.teixeira@vizir.com.br>
2020-05-11 13:53:53 -0300

    Add part3 exercise 3.2, and 3.2

--- part3/index.js
+++ part3/index.js
@@ -3,34 +3,49 @@ const app = express()
 
 let persons = [
     {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
     },
     {
+      "id": 2,
       "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
+      "number": "39-44-5323523"
     },
     {
+      "id": 3,
       "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
+      "number": "12-43-234345"
     },
     {
+      "id": 4,
       "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
+      "number": "39-23-6423122"
     },
     {
+      "id": 5,
       "name": "Alexandre",
-      "number": "11 988890105",
-      "id": 5
+      "number": "11 988890105"
     }
   ]
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+  res.send(`Phonebook has info for ${persons.length} people <br />
+  ${new Date()}`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
 })
 
 const port = 3001
