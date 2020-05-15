503df5f70410637e45a23aa629ef92858b719ce6
Alexandre Teixeira <alexandre.teixeira@vizir.com.br>
2020-05-14 23:30:31 -0300

    Part3 exercise 3.9 to 3.11, creating the back end and deploy to heroku

---
+++ part3/build/precache-manifest.3580204849f77d17cedc058e03003864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a7a644d1a93af4097a0967048ad847",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d175ab0a21c75c2249fb",
+    "url": "/static/css/main.52c7cd01.chunk.css"
+  },
+  {
+    "revision": "2b5fcf915faa7dc65c9f",
+    "url": "/static/js/2.da58ffa3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.da58ffa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d175ab0a21c75c2249fb",
+    "url": "/static/js/main.a64c59c7.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
