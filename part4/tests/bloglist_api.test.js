0ebd8419fab91ee67e19eb0326c135d3340a919a
Alexandre Teixeira <mrtalexandre@gmail.com>
2020-06-22 22:26:56 -0300

    Add test to add a blog and to be return as json

--- part4/tests/bloglist_api.test.js
+++ part4/tests/bloglist_api.test.js
@@ -4,13 +4,79 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test.only('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'titulo',
+    author: 'autor',
+    url: 'url.com.br',
+    likes: 100,
+    id: '5ee96ff91eb96bc0e57ee909'
+  },
+  {
+    title: 'O Livro de Alexandre O Grande',
+    author: 'Alexandre',
+    url: 'alexandre.com.br',
+    likes: 1000000,
+    id: '5ee970971eb96bc0e57ee90a'
+  },
+  {
+    title: 'O Livro da Refatoração',
+    author: 'Alexandre',
+    url: 'refactor.com',
+    likes: 12,
+    id: '5eead034c2b420e707d33167'
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+
+  blogObject = new Blog(initialBlogs[2])
+  await blogObject.save()
 })
 
-afterAll(() => {
-  mongoose.connection.close()
+describe('bloglist API', () => {
+
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Testando com supertest',
+      author: 'FullstackOpen',
+      url: 'fullstackopen.com',
+      likes: 1000,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const title = response.body.map(r => r.title)
+
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    expect(title).toContain(
+      'Testando com supertest'
+    )
+  })
+
+  afterAll(() => {
+    mongoose.connection.close()
+  })
 })
