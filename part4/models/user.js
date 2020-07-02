c21ca11c3c62204c20fc6f360c16f89780b4d494
Alexandre Teixeira <mrtalexandre@gmail.com>
2020-07-01 22:50:06 -0300

    Update user model validations

--- part4/models/user.js
+++ part4/models/user.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    unique: true,
+    validate: /[a-z0-9]/,
+    minlength: 4,
+  },
   name: String,
   passwordHash: String,
   blogs: [
@@ -11,6 +17,8 @@ const userSchema = new mongoose.Schema({
     }
   ],
 })
+
+userSchema.plugin(uniqueValidator)
 
 userSchema.set('toJSON', {
   transform:(document, returnedObject) => {
