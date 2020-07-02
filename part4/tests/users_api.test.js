25bd7d4770299ed91e4778f0d60db13658f644a8
Alexandre Teixeira <mrtalexandre@gmail.com>
2020-07-01 22:35:36 -0300

    Add tests for users endpoint

---
+++ part4/tests/users_api.test.js
@@ -0,0 +1,64 @@
+const bcrypt = require('bcrypt')
+
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+const User = require('../models/user')
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('senha', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'tedl',
+      name: 'Alexandre Teixeira',
+      password: 'google'
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(user => user.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('response with appropriate status code when username already exists in database', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'SuperUsuario',
+      password: 'monstro'
+    }
+
+    const result = await api
+      .post('api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('`username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+
+  })
+})
