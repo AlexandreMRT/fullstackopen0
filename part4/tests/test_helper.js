48f5e4f395ab31551b1c721c3bb90f5cae13c169
Alexandre Teixeira <mrtalexandre@gmail.com>
2020-07-12 03:39:15 -0300

    Refactor initialUsers and blogs

--- part4/tests/test_helper.js
+++ part4/tests/test_helper.js
@@ -3,30 +3,6 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 const config = require('../utils/config')
 
-const initialBlogs = [
-  {
-    title: 'titulo',
-    author: 'autor',
-    url: 'url.com.br',
-    likes: 100,
-    id: '5ee96ff91eb96bc0e57ee909'
-  },
-  {
-    title: 'O Livro de Alexandre O Grande',
-    author: 'Alexandre',
-    url: 'alexandre.com.br',
-    likes: 1000000,
-    id: '5ee970971eb96bc0e57ee90a'
-  },
-  {
-    title: 'O Livro da Refatoração',
-    author: 'Alexandre',
-    url: 'refactor.com',
-    likes: 12,
-    id: '5eead034c2b420e707d33167'
-  }
-]
-
 const initialUsers = [
   {
     _id: '5efd328c67b2ad967fcb5551',
@@ -41,6 +17,31 @@ const initialUsers = [
     name: 'Alexandre Teixeira',
     password: '123456',
     __v: 0
+  }
+]
+
+const initialBlogs = [
+  {
+    title: 'titulo',
+    author: 'autor',
+    url: 'url.com.br',
+    likes: 100,
+    id: '5ee96ff91eb96bc0e57ee909',
+    user: initialUsers[0]._id,
+  },
+  {
+    title: 'O Livro de Alexandre O Grande',
+    author: 'Alexandre',
+    url: 'alexandre.com.br',
+    likes: 1000000,
+    id: '5ee970971eb96bc0e57ee90a'
+  },
+  {
+    title: 'O Livro da Refatoração',
+    author: 'Alexandre',
+    url: 'refactor.com',
+    likes: 12,
+    id: '5eead034c2b420e707d33167'
   }
 ]
 
