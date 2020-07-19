0afa6bfd50a3109041e62fcf7426d74ba9d4a69f
Alexandre Teixeira <mrtalexandre@gmail.com>
2020-07-18 23:33:47 -0300

    Add create blog form component

---
+++ part5/src/components/CreateBlog.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const LoginForm = ({
+  handleSubmit,
+  handleBlogTitleChange,
+  handleBlogAuthorChange,
+  handleBlogUrlChange,
+  title,
+  author,
+  url,
+}) => {
+  return (
+    <div>
+      <h2>Create Blog</h2>
+
+      <form onSubmit={handleSubmit}>
+      <div>
+        Title
+        <input
+          type="text"
+          value={title}
+          name="Title"
+          onChange={handleBlogTitleChange}
+        />
+      </div>
+      <div>
+        Author
+        <input
+          type="text"
+          value={author}
+          name="Author"
+          onChange={handleBlogAuthorChange}
+        />
+      </div>
+      <div>
+        URL
+        <input
+          type="text"
+          value={url}
+          name="Url"
+          onChange={handleBlogUrlChange}
+        />
+      </div>
+      <button type="submit">Create</button>
+    </form>
+    </div>
+    
+  )
+}
+
+export default LoginForm
