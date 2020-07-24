f7cfc1ce98bc89adb78f7e488d2bc12598845501
Alexandre Teixeira <mrtalexandre@gmail.com>
2020-07-23 23:56:58 -0300

    Update blog component to support a togglable button to display the details of a blog

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,27 @@
 import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
+import Togglable from './Togglable';
+const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  return (
+
+  <div style={blogStyle}>
     {blog.title} {blog.author}
-  </div>
-)
+
+    <Togglable buttonLabel={"View"}>
+      <div>
+        {blog.url}<br/>
+        {blog.likes}<br/>
+      </div>
+    </Togglable>
+    </div>
+  )
+}
 
 export default Blog
