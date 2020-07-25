02ce7af2765df526cc0a54ea499cec13c9d8be78
Alexandre Teixeira <mrtalexandre@gmail.com>
2020-07-24 23:50:28 -0300

    Refactor Blog Form component

--- part5/src/components/BlogForm.js
+++ part5/src/components/BlogForm.js
@@ -21,39 +21,39 @@ const BlogForm = ({ createBlog }) => {
   return (
     <div>
       <h2>Create Blog</h2>
-
       <form onSubmit={addBlog}>
-      <div>
-        Title
-        <input
-          type="text"
-          value={title}
-          name="title"
-          onChange={({ target }) => setTitle(target.value)}
-        />
-      </div>
-      <div>
-        Author
-        <input
-          type="text"
-          value={author}
-          name="author"
-          onChange={({ target }) => setAuthor(target.value)}
-        />
-      </div>
-      <div>
-        URL
-        <input
-          type="text"
-          value={url}
-          name="url"
-          onChange={({ target }) => setUrl(target.value)}
-        />
-      </div>
-      <button type="submit">Create</button>
-    </form>
+        <div>
+          Title
+          <input
+            type="text"
+            value={title}
+            name="title"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+
+        <div>
+          Author
+          <input
+            type="text"
+            value={author}
+            name="author"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+
+        <div>
+          URL
+          <input
+            type="text"
+            value={url}
+            name="url"
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">Create</button>
+      </form>
     </div>
-    
   )
 }
 
