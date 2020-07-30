231ef6dbcada51f8bf5f8d0afdba516548c7f479
Alexandre Teixeira <mrtalexandre@gmail.com>
2020-07-30 18:01:15 -0300

    Add Prop types to togglable component

--- part5/src/components/Togglable.js
+++ part5/src/components/Togglable.js
@@ -28,6 +28,12 @@ const Togglable = React.forwardRef((props, ref) => {
       </div>
     </div>
   )
+})
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
 }
+
+Togglable.displayName = 'Togglable'
 
 export default Togglable
