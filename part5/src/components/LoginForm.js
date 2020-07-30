9749aaeb862b533cfedc4336ea0033ef25bb136f
Alexandre Teixeira <mrtalexandre@gmail.com>
2020-07-30 18:01:55 -0300

    Add Prop types to login form component

--- part5/src/components/LoginForm.js
+++ part5/src/components/LoginForm.js
@@ -32,11 +32,19 @@ const LoginForm = ({
             onChange={handlePasswordChange}
           />
         </div>
-      <button type="submit">Login</button>
+        <button type="submit">Login</button>
       </form>
     </div>
-    
+
   )
+}
+
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
 }
 
 export default LoginForm
