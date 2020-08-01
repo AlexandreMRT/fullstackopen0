a56bcddf22901f845838154c7b87c882be3b4efe
Alexandre Teixeira <mrtalexandre@gmail.com>
2020-07-31 23:03:46 -0300

    Add comments to future improvments

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -41,3 +41,57 @@ const Blog = ({ blog }) => {
 }
 
 export default Blog
+
+
+
+//OUTRA FORMA DE ESCREVER ESSE COMPONENTE
+
+
+// import React, { useState } from 'react'
+// import blogService from '../services/blogs'
+
+// const Blog = ({ blog }) => {
+//   const [visible, setVisible] = useState(false)
+
+//   const toggleVisibility = () => {
+//     setVisible(!visible)
+//   }
+
+//   const blogStyle = {
+//     paddingTop: 10,
+//     paddingLeft: 2,
+//     border: 'solid',
+//     borderWidth: 1,
+//     marginBottom: 5
+//   }
+
+//   const removeButtonStyle = {
+//     backgroundColor: 'blue',
+//     borderRadius: '5px',
+//     color: 'white',
+//     fontSize: '15px'
+//   }
+
+//   return (
+//     <div className='blog'>
+//       {visible ?
+//         <div style={blogStyle}>
+//           {blog.title} {blog.author} <button onClick={toggleVisibility}>hide</button>
+//           <br />
+//           {blog.url}
+//           <br />
+//             likes: {blog.likes} <button onClick={() => blogService.update(blog.id, { likes: blog.likes + 1 })}>like</button>
+//           <br />
+//           {blog.user.username}
+//           <br />
+//           <button style={removeButtonStyle} onClick={() => { if(window.confirm('Delete the item?')) {blogService.remove(blog.id)} }}>delete</button>
+//         </div> :
+//         <div style={blogStyle}>
+//           {blog.title} {blog.author} <button onClick={toggleVisibility}>view</button>
+//         </div>
+//       }
+//     </div>
+//   )
+// }
+
+// export default Blog
