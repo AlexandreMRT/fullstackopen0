3d56060b2c31c46da3544d5c5d6d47da3011dd48
Alexandre Teixeira <mrtalexandre@gmail.com>
2020-08-01 23:05:50 -0300

    Refactor the blog component to properlly togglable the information

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -4,8 +4,6 @@ import blogService from '../services/blogs'
 const Blog = ({ blog }) => {
   const [visible, setVisible] = useState(false)
 
-  const showWhenVisible = { display: visible ? '' : 'none' }
-
   const toggleVisibility = () => {
     setVisible(!visible)
   }
@@ -26,72 +24,25 @@ const Blog = ({ blog }) => {
   }
 
   return (
-    <div className='blog' style={blogStyle}>
-      {blog.title} {blog.author}
-      <button onClick={toggleVisibility}>{ visible ? 'Hide' : 'Show' }</button>
-
-      <div style={showWhenVisible}>
-        {blog.url}<br />
-        likes {blog.likes} <button onClick={() => blogService.update(blog.id, { likes: blog.likes + 1 })} >Like</button><br/>
-        {blog.user.name}<br />
-        <button style={removeButtonStyle} onClick={() => { if(window.confirm('Delete the item?')) {blogService.remove(blog.id)} }} >Remove</button><br/>
-      </div>
+    <div className='blog'>
+      {visible ?
+        <div style={blogStyle}>
+          {blog.title} {blog.author} <button onClick={toggleVisibility}>Hide</button>
+          <br />
+          {blog.url}
+          <br />
+            Likes: {blog.likes} <button onClick={() => blogService.update(blog.id, { likes: blog.likes + 1 })}>Like</button>
+          <br />
+          {blog.user.username}
+          <br />
+          <button style={removeButtonStyle} onClick={() => { if(window.confirm('Delete the item?')) {blogService.remove(blog.id)} }}>Remove</button>
+        </div> :
+        <div style={blogStyle}>
+          {blog.title} {blog.author} <button onClick={toggleVisibility}>Show</button>
+        </div>
+      }
     </div>
   )
 }
 
 export default Blog
-
-
-
-//OUTRA FORMA DE ESCREVER ESSE COMPONENTE
-
-
-// import React, { useState } from 'react'
-// import blogService from '../services/blogs'
-
-// const Blog = ({ blog }) => {
-//   const [visible, setVisible] = useState(false)
-
-//   const toggleVisibility = () => {
-//     setVisible(!visible)
-//   }
-
-//   const blogStyle = {
-//     paddingTop: 10,
-//     paddingLeft: 2,
-//     border: 'solid',
-//     borderWidth: 1,
-//     marginBottom: 5
-//   }
-
-//   const removeButtonStyle = {
-//     backgroundColor: 'blue',
-//     borderRadius: '5px',
-//     color: 'white',
-//     fontSize: '15px'
-//   }
-
-//   return (
-//     <div className='blog'>
-//       {visible ?
-//         <div style={blogStyle}>
-//           {blog.title} {blog.author} <button onClick={toggleVisibility}>hide</button>
-//           <br />
-//           {blog.url}
-//           <br />
-//             likes: {blog.likes} <button onClick={() => blogService.update(blog.id, { likes: blog.likes + 1 })}>like</button>
-//           <br />
-//           {blog.user.username}
-//           <br />
-//           <button style={removeButtonStyle} onClick={() => { if(window.confirm('Delete the item?')) {blogService.remove(blog.id)} }}>delete</button>
-//         </div> :
-//         <div style={blogStyle}>
-//           {blog.title} {blog.author} <button onClick={toggleVisibility}>view</button>
-//         </div>
-//       }
-//     </div>
-//   )
-// }
-
-// export default Blog
