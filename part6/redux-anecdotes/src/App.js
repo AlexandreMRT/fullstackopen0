36f8dce1c9443885a2598db900f73b6b2944aa1b
Alexandre <mrtalexandre@gmail.com>
2020-09-15 23:04:10 -0300

    Refactor app to use components

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,49 +1,14 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { createStore } from 'redux';
-import { addAnecdote, voteForAnecdote } from './reducers/anecdoteReducer'
-
-import reducer from './reducers/anecdoteReducer';
-
-
-const store = createStore(reducer)
+import Anecdotes from './components/Anecdotes'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  console.log('anecdotes :>> ', anecdotes);
-  const dispatch = useDispatch()
-
-  const voteAnecdote = ( id ) => {
-    dispatch(voteForAnecdote(id))
-  }
-
-  const createAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    store.dispatch(addAnecdote(content))
-  }
 
   return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => voteAnecdote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form onSubmit={createAnecdote}>
-        <div><input name="anecdote" /></div>
-        <button type="submit">create</button>
-      </form>
-    </div>
+    <>
+      <Anecdotes />
+      <AnecdoteForm />
+    </>
   )
 }
 
