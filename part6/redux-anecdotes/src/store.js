edb5c237579ed93d2395b58b5388cc524d65cabe
Alexandre <mrtalexandre@gmail.com>
2020-09-29 23:41:23 -0300

    Add the notification reducer as a combined reducer

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -2,10 +2,11 @@ import { createStore, combineReducers } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import AnecdoteReducer from './reducers/anecdoteReducer'
-
+import NotificationReducer from './reducers/notificationReducer'
 
 const reducer = combineReducers({
   anecdotes: AnecdoteReducer,
+  notifications: NotificationReducer,
 })
 
 const store = createStore(
