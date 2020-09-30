ee656bb13dce3ee8b462359bfff6119e50cefdeb
Alexandre <mrtalexandre@gmail.com>
2020-09-29 23:40:48 -0300

    Add the notifications component to the main app

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import AnecdotesList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
 
 const App = () => {
 
   return (
     <>
+      <Notification />
       <AnecdotesList />
       <AnecdoteForm />
     </>
