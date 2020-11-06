a5085f7d01426bc9fcb00d409f896e98692897ba
Alexandre <mrtalexandre@gmail.com>
2020-11-05 23:56:44 -0300

    WIP 6.19

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+// import { useSelector, useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { voteForAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
 import Filter from './Filter'
@@ -15,35 +16,71 @@ const Anecdote = ({ anecdote, handleClick }) => {
   )
 }
 
-const Anecdotes = () => {
-  const dispatch = useDispatch()
+const Anecdotes = (props) => {
+  // const dispatch = useDispatch()
 
-  const anecdotes = useSelector(state => {
-    if ( state.filter === '' ) {
-      return state.anecdotes
-    } else {
-      return (state.anecdotes.filter(anecdote => anecdote.content.toUpperCase().includes(state.filter.toUpperCase())))
-    }
-  })
+  // const anecdotes = useSelector(state => {
+  //   if ( state.filter === '' ) {
+  //     return state.anecdotes
+  //   } else {
+  //     return (state.anecdotes.filter(anecdote => anecdote.content.toUpperCase().includes(state.filter.toUpperCase())))
+  //   }
+  // })
 
-  const voteFor = async (anecdote) => {
-    dispatch(voteForAnecdote(anecdote))
-    dispatch(setNotification(`you voted '${anecdote.content}'`, 3))
-  }
+  // const voteFor = async (anecdote) => {
+  //   dispatch(voteForAnecdote(anecdote))
+  //   dispatch(setNotification(`you voted '${anecdote.content}'`, 3))
+  // }
+
+  // MEIO TERMO
+  // const anecdotes = () => {
+  //   if ( props.filter === '' ) {
+  //     return props.anecdotes
+  //   } else {
+  //     return (props.anecdotes.filter(anecdote => anecdote.content.toUpperCase().includes(props.filter.toUpperCase())))
+  //   }
+  // }
+
 
   return (
     <div>
       <h2>Anecdotes</h2>
       <Filter />
-      {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+      {props.anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
         <Anecdote
           key={anecdote.id}
           anecdote={anecdote}
-          handleClick={() => voteFor(anecdote)}
+          handleClick={ () => {
+              props.voteForAnecdote(anecdote)
+              props.setNotification(`you voted '${anecdote.content}'`, 3)
+            }
+          }
         />
       )}
     </div>
   )
 }
 
-export default Anecdotes
+const mapStateToProps = (state) => {
+  if ( state.filter === '' ) {
+    return {
+      anecdotes: state.anecdotes
+    }
+  } else {
+    return {
+      anecdotes: (state.anecdotes.filter(anecdote => anecdote.content.toUpperCase().includes(state.filter.toUpperCase())))
+    }
+  }
+}
+
+const mapDispatchToProps = {
+  voteForAnecdote,
+  setNotification
+}
+
+const ConnectedAnecdotes = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Anecdotes)
+
+export default ConnectedAnecdotes
