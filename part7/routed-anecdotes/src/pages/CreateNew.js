4d3d26c527d92630c18c24a00beb045205fd2323
Alexandre <mrtalexandre@gmail.com>
2020-11-25 23:45:12 -0300

    7.5/7.6 Add reset button using spread operator

--- part7/routed-anecdotes/src/pages/CreateNew.js
+++ part7/routed-anecdotes/src/pages/CreateNew.js
@@ -1,21 +1,31 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect, useHistory } from "react-router-dom"
+import { useField } from '../hooks/index';
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
   const history = useHistory()
+
+  const withoutReset = (({ reset, ...rest }) => rest)
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     history.push('/')
+  }
+
+  const handleReset = (event) => {
+    event.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
   }
 
   return (
@@ -24,17 +34,19 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...withoutReset(content)} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...withoutReset(author)} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...withoutReset(info)} />
         </div>
+
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
