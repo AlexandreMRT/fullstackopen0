85466ebff726f4ba3b21a51a0a564705f3042546
Alexandre <mrtalexandre@gmail.com>
2021-01-29 23:18:51 -0300

    Add setnotification action to reducer

--- part5/src/reducers/NotificationReducer.js
+++ part5/src/reducers/NotificationReducer.js
@@ -1,8 +1,23 @@
-const NotificationReducer = (state = [], action) => {
-  console.log('action.type :>> ', action.type)
+export const setNotification = (content, type, time) => {
+  return async dispatch => {
+    dispatch({
+      type: 'SET_NOTIFICATION',
+      data: { content, type }
+    })
+    setTimeout(() => {
+      dispatch({
+        type: 'CLEAR_NOTIFICATION'
+      })
+    }, time * 1000)
+  }
+}
+
+const NotificationReducer = (state = null, action) => {
   switch (action.type) {
-  case 'NEW_BLOG':
-    return [...state, action.data]
+  case 'SET_NOTIFICATION':
+    return action.data
+  case 'CLEAR_NOTIFICATION':
+    return null
   default:
     return state
   }
