dfa14343d3536f3ad82eb3b603ff3da1af9790e6
Alexandre <mrtalexandre@gmail.com>
2021-01-29 23:20:30 -0300

    Add redux thunk and redux dev tools

--- part5/src/store.js
+++ part5/src/store.js
@@ -1,7 +1,17 @@
-import { createStore } from 'redux'
-
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import NotificationReducer from './reducers/NotificationReducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store = createStore(NotificationReducer)
+const reducer = combineReducers({
+  notifications: NotificationReducer
+})
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+)
 
 export default store
