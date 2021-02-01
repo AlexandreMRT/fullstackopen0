d17fada172c1cc25bd3fa6f95d8a601cc16b11a6
Alexandre <mrtalexandre@gmail.com>
2021-01-31 23:12:46 -0300

    WIP Add blogs reducer

--- part5/src/store.js
+++ part5/src/store.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import NotificationReducer from './reducers/NotificationReducer'
+import BlogsReducer from './reducers/BlogsReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 const reducer = combineReducers({
-  notifications: NotificationReducer
+  notifications: NotificationReducer,
+  blogs: BlogsReducer
 })
 
 const store = createStore(
