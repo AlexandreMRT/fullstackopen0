2727fa217ef86885bb5641ed8321662b8f7ba8c8
Alexandre <mrtalexandre@gmail.com>
2021-02-04 23:49:58 -0300

    Add user reducer to store

--- part5/src/store.js
+++ part5/src/store.js
@@ -3,10 +3,12 @@ import thunk from 'redux-thunk'
 import NotificationReducer from './reducers/NotificationReducer'
 import BlogsReducer from './reducers/BlogsReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import UserReducer from './reducers/UserReducer'
 
 const reducer = combineReducers({
   notifications: NotificationReducer,
-  blogs: BlogsReducer
+  blogs: BlogsReducer,
+  user: UserReducer,
 })
 
 const store = createStore(
