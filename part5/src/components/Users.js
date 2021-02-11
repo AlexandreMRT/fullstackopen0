967bd99e28f3d82fd87d10f05ad929ab6693f9e0
Alexandre <mrtalexandre@gmail.com>
2021-02-11 01:34:12 -0300

    Add data fetching to users component

--- part5/src/components/Users.js
+++ part5/src/components/Users.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getUsers, initializeUsers } from '../reducers/UsersReducer'
+import { useSelector, useDispatch } from 'react-redux'
+import User from './User/User'
 
 const Users = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeUsers())
+  },[dispatch])
+
+  const users = useSelector(getUsers)
+
+  const userStyle = {
+    paddingLeft: 150,
+    marginBottom: 5,
+    fontWeight: 'bold'
+  }
 
   return (
-    <div>AEAEAEA</div>
+    <div>
+      <h2>Users</h2>
+      <p style={userStyle}>blogs created</p>
+      <div>
+        {
+          users.sort((a, b) => b.blogs.length - a.blogs.length).map(user =>
+            <User key={user.id} user={user} />
+          )
+        }
+      </div>
+    </div>
   )
 }
 
