7ec68b34bb9bf346c58f58159ddaf0a2a5dc526d
Alexandre <mrtalexandre@gmail.com>
2021-02-11 23:57:16 -0300

    Fix routes to users and single user

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import BlogList from './components/BlogList/BlogList'
 import Users from './components/Users'
+import { initializeUsers, getUsers } from './reducers/UsersReducer'
+import User from './components/User/User'
 
 const App = () => {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeUsers())
+  },[dispatch])
+
+  const users = useSelector(getUsers)
+
   return(
     <Router>
       <div>
@@ -16,8 +28,12 @@ const App = () => {
           <BlogList />
         </Route>
 
-        <Route path="/users">
+        <Route exact path="/users">
           <Users />
+        </Route>
+
+        <Route path="/users/:id">
+          <User users={users} />
         </Route>
       </Switch>
     </Router>
