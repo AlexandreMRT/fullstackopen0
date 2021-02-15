17e7baaee5d9ee9389efce290cc016f802c439e9
Alexandre <mrtalexandre@gmail.com>
2021-02-14 22:55:31 -0300

    User component now return the blogs titles in a list

--- part5/src/components/User/User.js
+++ part5/src/components/User/User.js
@@ -11,11 +11,9 @@ const User = ({ users }) => {
       <h1>{user?.name}</h1>
       <h2>added blogs</h2>
       <ul>
-        {
-          user?.blogs.map(blog => {
-            console.log(blog.title)
-          })
-        }
+        {user?.blogs.map(blog => {
+          return <li key={blog.id}>{blog.title}</li>
+        })}
       </ul>
     </div>
   )
