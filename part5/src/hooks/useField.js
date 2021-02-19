782cd29fa3c15148078ec19451b93946e26bf3d8
Alexandre <mrtalexandre@gmail.com>
2021-02-18 21:59:48 -0300

    Add a reset field feature

--- part5/src/hooks/useField.js
+++ part5/src/hooks/useField.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 export const useField = (type) => {
   const [value, setValue] = useState('')
@@ -7,9 +7,14 @@ export const useField = (type) => {
     setValue(event.target.value)
   }
 
+  const reset = () => {
+    setValue('')
+  }
+
   return {
     type,
     value,
-    onChange
+    onChange,
+    reset
   }
 }
