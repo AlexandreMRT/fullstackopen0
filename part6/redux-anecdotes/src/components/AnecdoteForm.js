d6acb677b03cb4c617d2384f2297987e5556c3e7
Alexandre <mrtalexandre@gmail.com>
2021-03-03 22:59:33 -0300

    Fix conflicts

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { addAnecdote } from '../reducers/anecdoteReducer'
-import { setNotification } from '../reducers/notificationReducer'
+import React from 'react';
+import { addAnecdote } from '../reducers/anecdoteReducer';
+import { setNotification } from '../reducers/notificationReducer';
+import { connect } from 'react-redux';
 
 const AnecdoteForm = (props) => {
-
-  const createAnecdote = async (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    props.addAnecdote(content)
-    props.setNotification(`new anecdote '${content}'`, 5)
-  }
+  const create = async (event) => {
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = '';
+    props.addAnecdote(content);
+    props.setNotification(`new anecdote '${content}'`, 5);
+  };
 
   return (
     <>
       <h2>create new</h2>
-      <form onSubmit={createAnecdote}>
-        <input name='anecdote' />
-        <button type='submit'>create</button>
+      <form onSubmit={create}>
+        <div>
+          <input name='anecdote' />
+        </div>
+        <button>create</button>
       </form>
     </>
-  )
-}
+  );
+};
 
-export default connect(
-  null,
-  { addAnecdote, setNotification }
-)(AnecdoteForm)
+export default connect(null, { addAnecdote, setNotification })(AnecdoteForm);
