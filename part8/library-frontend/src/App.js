5d4c0ba90aae8442be96620d293d32ad6e31b7ac
Alexandre <mrtalexandre@gmail.com>
2021-03-22 23:46:29 -0300

    Add authors component to recieve graph result

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,12 +1,18 @@
-
-import React, { useState } from 'react'
-import Authors from './components/Authors'
-import Books from './components/Books'
-import NewBook from './components/NewBook'
+import React, { useState } from 'react';
+import Authors from './components/Authors';
+import Books from './components/Books';
+import NewBook from './components/NewBook';
+import { useQuery } from '@apollo/client';
+import { ALL_AUTHORS } from './queries';
 
 const App = () => {
-  const [page, setPage] = useState('authors')
+  const [page, setPage] = useState('authors');
 
+  const result = useQuery(ALL_AUTHORS);
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
   return (
     <div>
       <div>
@@ -15,20 +21,13 @@ const App = () => {
         <button onClick={() => setPage('add')}>add book</button>
       </div>
 
-      <Authors
-        show={page === 'authors'}
-      />
+      <Authors show={page === 'authors'} authors={result.data?.allAuthors} />
 
-      <Books
-        show={page === 'books'}
-      />
+      <Books show={page === 'books'} />
 
-      <NewBook
-        show={page === 'add'}
-      />
-
+      <NewBook show={page === 'add'} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
