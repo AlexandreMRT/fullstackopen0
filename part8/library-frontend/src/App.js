578ebe62fcb7434710a803b31a731f46fd6fce76
Alexandre <mrtalexandre@gmail.com>
2021-03-24 23:56:21 -0300

    Set the queries to pool every 2 seconds

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -8,8 +8,13 @@ import { ALL_AUTHORS, ALL_BOOKS } from './queries';
 const App = () => {
   const [page, setPage] = useState('authors');
 
-  const authorsResult = useQuery(ALL_AUTHORS);
-  const booksResult = useQuery(ALL_BOOKS);
+  const authorsResult = useQuery(ALL_AUTHORS, {
+    pollInterval: 2000,
+  });
+
+  const booksResult = useQuery(ALL_BOOKS, {
+    pollInterval: 2000,
+  });
 
   if (authorsResult.loading || booksResult.loading) {
     return <div>loading...</div>;
